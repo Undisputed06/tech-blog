842960902dfde7a089a5ab757955b7cb2afdeeb4
Gary <52983305+Undisputed06@users.noreply.github.com>
2022-06-21 19:15:19 -0500

    homepage handlebars

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -4,7 +4,7 @@ const { Post, User, Comment } = require('../../models');
 router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
-      attributes: ['id','post_url','title','created_at',],
+      attributes: ['id','post_text','title','created_at',],
       order: [['created_at', 'DESC']],
       include: [
         {
@@ -33,7 +33,7 @@ router.get('/', (req, res) => {
       where: {
         id: req.params.id
       },
-      attributes: ['id', 'post_url', 'title', 'created_at'],
+      attributes: ['id', 'post_text', 'title', 'created_at'],
       include: [
         {
           model: Comment,
@@ -66,7 +66,7 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     Post.create({
       title: req.body.title,
-      post_url: req.body.post_url,
+      post_text: req.body.post_text,
       user_id: req.body.user_id
     })
       .then(dbPostData => res.json(dbPostData))
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({})
 
 const app = express();
 const PORT = process.env.Port ||3001;
@@ -10,6 +12,9 @@ app.use(express.urlencoded({ extended: true}));
 app.use(require('./controllers'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-sequelize.sync({ force: false }).then(() => {
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
+
+sequelize.sync({ force: true}).then(() => {
     app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
   });
