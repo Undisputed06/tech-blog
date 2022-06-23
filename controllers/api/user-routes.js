682d4cbb38cfefece0df22c22bbeadff15f257ab
Gary <52983305+Undisputed06@users.noreply.github.com>
2022-06-22 19:50:43 -0500

    fixed login functionality

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -55,21 +55,25 @@ router.get('/:id', (req, res) => {
       email: req.body.email,
       password: req.body.password
     })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+    
+        res.json(dbUserData);
+      });
+    })
+  })
 
 router.post('/login', (req, res)=> {
   User.findOne({
     where: {
-      email: req.body.email
+      username: req.body.username
     }
   }).then(dbUserData =>{
     if(!dbUserData){
-      res.status(400).json({ message: 'No user with that email address' });
+      res.status(400).json({ message: 'No user with that username' });
       return
     }
    
@@ -80,9 +84,26 @@ router.post('/login', (req, res)=> {
       res.status(400).json({ message:'Incorrect password!' })
       return
     }
-    res.json({ user: dbUserData, message: 'You are now logged in!'})
-  
-  })
+      req.session.save(() => {
+      //declare session variables
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+        
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+});
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
 })
 
 // PUT /api/users/1
--- server.js
+++ server.js
@@ -3,9 +3,25 @@ const express = require('express');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({})
+const session = require('express-session')
+
 
 const app = express();
 const PORT = process.env.Port ||3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
@@ -15,6 +31,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
 
-sequelize.sync({ force: true}).then(() => {
+sequelize.sync({ force: false}).then(() => {
     app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
   });
